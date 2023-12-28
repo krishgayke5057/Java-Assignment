fb681f7e6904929aac058dcf6665a8b0ee3422f2
Krushna Gayke <121627622+krishgayke5057@users.noreply.github.com>
2023-12-28 14:54:45 +0530

    Add files via upload

---
+++ Assignment 1 solution.java
@@ -0,0 +1,69 @@
+Q.1
+byte 
+short 
+int 
+long 
+double
+float
+chat
+String (we can say it primitive type)
+
+
+Q.2
+Class Name 
+Method Name
+Variable Name 
+
+Q.3
+Final Class = final class is a class that cannot be extended or subclassed. 
+ It can applied at e place
+1- Variable level
+2- method level 
+3- class level
+
+
+Q.4
+1-final class Dog
+{
+}
+
+2- class Dog
+{
+private Dog()
+{
+}
+}
+
+
+
+Q.5
+Yes we can create an instance of final class in another class
+
+
+
+Q.6 volatile Keywords=
+only one thread should access the variable value 
+
+Q.7 (not teach)
+
+Q.8
+1. Implicit Typecasting
+2. Explicit Typecasting 
+
+
+Q.9
+Boxing = : It is the process of converting a primitive data type into its corresponding wrapper class.
+
+Unboxing = : It is the process of converting an object of a wrapper class to its corresponding primitive data type.
+
+
+Q.10
+Keywords = It's is reserved word in java we can't use as a identifier
+there are total 53 reserved keywords in java .
+
+
+Literala = Literals are representations of constant values in the source code. 
+ex- 1, true , 3.15f
+
+Identifier = name giver to class, method, Variable are call identifier.
+ex- int a, class demo.
